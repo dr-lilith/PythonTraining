981f6f4e4fb0a3a67ab3ae6f2a4a17be035cef57
Irina <ireana.melnikova@gmail.com>
2021-10-03 22:58:39 +0300

    3 задачи 4 урока на циклы for

---
+++ PythonFor.py
@@ -0,0 +1,21 @@
+#
+# a = input("input the string:")
+# b = input("input the symbol")
+# for i in a:
+#     if b == i:
+#         c = a.replace(b, "")
+# print(c)
+
+
+
+# a = int(input("the begining"))
+# b = int(input("the end"))
+# c = int(input("the step"))
+# for i in range (a, b+1, c):
+#     print(i)
+
+
+
+for i in range (55, 9185, 5):
+    print(i)
+
