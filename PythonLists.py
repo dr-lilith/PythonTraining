260294400c6085c02283e786f84b3d8ee6a80842
Irina <ireana.melnikova@gmail.com>
2021-10-05 15:50:13 +0300

    4 задачи lists + examples

---
+++ PythonLists.py
@@ -0,0 +1,69 @@
+# a = [1, 2, 3, 4, 5, 6, 7, 8]
+# for i in a:
+#     if i <= 5:
+#         print(i)
+#
+#
+
+# a = []
+# i = 0
+# while i < 17:
+#     a.append(0)
+#     if i == 0 or i == 16:
+#         a[i] = 1
+#     i += 1
+# print(a)
+
+
+# lst = []
+# i = 0
+# while i < 5:
+#     lst.append(int(input("input the number of the list:")))
+#     i += 1
+# print(lst)
+# max_num = lst[0]
+# for l in lst:
+#     if l > max_num:
+#         max_num = l
+# print(max_num)
+
+
+# a = []
+# i = 1
+# while i < 25:
+#     if i % 2 == 0:
+#         a.append(i)
+#     i += 1
+# print(a)
+#
+# s = 0
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 11, 12, 13, 14, 15, 16]
+# s = sum(a)
+# print(s)
+
+# s = 1
+# a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16]
+# for i in a:
+#     s = s * i
+# print(s)
+
+
+
+# a = [2, 3, 4, 5, 6, 7, 8, 9, -10, 11, 12, 1, 13, 14, 15, 16]
+# s = a[0]
+# for i in a:
+#     if i < s:
+#         s = i
+# print(s)
+
+
+# a = [2, 3, 4, 5, 6, 7, 8, 9, -10, 11, 12, 1, 13, 14, 15, 16]
+# s = min(a)
+# print(s)
+#
+# a = [2, 3, 4, 5, 6, 7, 8, 9, -10, 11, 12, 1, 13, 14, 15, 16]
+# s = max(a)
+# print(s)
+
+
+
