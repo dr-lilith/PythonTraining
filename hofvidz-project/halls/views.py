8e83ed5e01ec46d5493459bf4621b85bc2545ee5
Irina <ireana.melnikova@gmail.com>
2022-02-28 17:22:34 +0300

    Video 31 passed. Home and Authentication.

--- hofvidz-project/halls/views.py
+++ hofvidz-project/halls/views.py
@@ -15,7 +15,9 @@ YOUTUBE_API_KEY = 'AIzaSyCMuWJvUpr8VScEVm9SRHM0xkwaCZgx3Rs'
 
 # Create your views here.
 def home(request):
-    return render(request, 'halls/home.html')
+    recent_halls = Hall.objects.all().order_by('-id')[:3]
+    popular_halls = [Hall.objects.get(pk=1),Hall.objects.get(pk=2),Hall.objects.get(pk=3)]
+    return render(request, 'halls/home.html', {'recent_halls':recent_halls, 'popular_halls':popular_halls})
 
 def dashboard(request):
     halls = Hall.objects.filter(user=request.user)
