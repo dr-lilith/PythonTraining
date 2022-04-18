b25ed1058d5d8874b0ce3ac230eb20ac3d9b1d29
Irina <ireana.melnikova@gmail.com>
2022-04-18 13:18:42 +0300

    Lecture 23 passed. Login Tokens

--- django3-todowoo-project/api/views.py
+++ django3-todowoo-project/api/views.py
@@ -8,6 +8,8 @@ from django.http import JsonResponse
 from django.db import IntegrityError
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate
+
 
 
 
@@ -27,10 +29,15 @@ def signup(request):
 def login(request):
     if request.method == 'POST':
         data = JSONParser().parse(request)
-        user = User.objects.create_user(data['username'], password=data['password'])
-        user.save()
-        token = Token.objects.create(user=user)
-        return JsonResponse({'token':str(token)}, status=201)
+        user = authenticate(request, username=request.POST['username'], password=data['password'])
+        if user is None:
+            return JsonResponse({'error':'Could not login. Please check username and password'}, status=400)
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)}, status=200)
 
 
 class TodoCompletedList(generics.ListAPIView):
